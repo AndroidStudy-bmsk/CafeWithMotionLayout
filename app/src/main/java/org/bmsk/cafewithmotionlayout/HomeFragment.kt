df4875ad2eb37cf7f458465e9c52a985c383bbc3
bmsk <vv99911@gmail.com>
2023-05-10 23:50:45 +0900

    Update HomeFragment.kt

--- app/src/main/java/org/bmsk/cafewithmotionlayout/HomeFragment.kt
+++ app/src/main/java/org/bmsk/cafewithmotionlayout/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import org.bmsk.cafewithmotionlayout.data.model.Home
+import org.bmsk.cafewithmotionlayout.data.model.Menu
 import org.bmsk.cafewithmotionlayout.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -14,7 +16,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         binding = FragmentHomeBinding.bind(view)
 
-        val homeData = context?.readData() ?: return
+        val homeData = context?.readData("home.json", Home::class.java) ?: return
+        val menuData = context?.readData("menu.json", Menu::class.java) ?: return
+
         binding.appBarTitleTextView.text =
             getString(R.string.appbar_title_text, homeData.user.nickname)
 
@@ -22,17 +26,40 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             R.string.appbar_start_title, homeData.user.startCount, homeData.user.totalCount
         )
 
+        binding.appbarProgressBar.progress = homeData.user.startCount
+        binding.appbarProgressBar.max = homeData.user.totalCount
+
         Glide.with(binding.appBarImageView)
             .load(homeData.appbarImage)
             .into(binding.appBarImageView)
 
-        binding.appbarProgressBar.progress = homeData.user.startCount
-        binding.appbarProgressBar.max = homeData.user.totalCount
-        binding.recommendMenuList.menuLinearLayout.addView(
-            MenuView(context = requireContext()).apply {
-                setTitle("디카페인 카페라떼")
-                setImageUrl("https://picsum.photos/200/300")
-            }
-        )
+        binding.recommendMenuList.titleTextView.text =
+            getString(R.string.recommend_title, homeData.user.nickname)
+        menuData.coffee.forEach { menuItem ->
+            binding.recommendMenuList.menuLinearLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageUrl(menuItem.image)
+                }
+            )
+        }
+
+        binding.bannerLayout.bannerImageView.apply {
+            Glide.with(this)
+                .load(homeData.banner.image)
+                .into(this)
+            this.contentDescription = homeData.banner.contentDescription
+        }
+
+        binding.foodMenuList.titleTextView.text =
+            getString(R.string.food_menu_title)
+        menuData.food.forEach { menuItem ->
+            binding.foodMenuList.menuLinearLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageUrl(menuItem.image)
+                }
+            )
+        }
     }
 }
