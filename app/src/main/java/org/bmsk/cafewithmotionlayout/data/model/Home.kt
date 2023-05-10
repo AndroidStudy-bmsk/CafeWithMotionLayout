c0e7bf1139902f8bd6b48167b7176c43cf680ec1
bmsk <vv99911@gmail.com>
2023-05-10 23:50:19 +0900

    Update Home.kt

--- app/src/main/java/org/bmsk/cafewithmotionlayout/data/model/Home.kt
+++ app/src/main/java/org/bmsk/cafewithmotionlayout/data/model/Home.kt
@@ -3,10 +3,16 @@ package org.bmsk.cafewithmotionlayout.data.model
 data class Home(
     val user: User,
     val appbarImage: String,
+    val banner: Banner
 )
 
 data class User(
     val nickname: String,
     val startCount: Int,
     val totalCount: Int,
+)
+
+data class Banner(
+    val image: String,
+    val contentDescription: String
 )
